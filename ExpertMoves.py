c8e36e23b9826b6f40e511f70e29de0e3b73bf09
leonardo1904 <103266551+leonardo1904@users.noreply.github.com>
2022-04-08 16:51:33 +0100

    pseudo code for expert bot 2 mvs

---
+++ ExpertMoves.py
@@ -0,0 +1,21 @@
+# move 1
+
+# if computer is x, take a corner, probably some randint from 1 to 4
+# if computer is o, take the center, if open. If x takes center, take a corner
+
+# move 2
+
+# X: if o put it in the center, place x in the corner opposite to 1st x
+# if o put it in any corner, put x in any corner
+# if o put in on a side, put x in center
+
+# O: block X if needed
+# if there are two X on opposite corner with o  in middle, put in on a side
+# if 1xcorner,2omiddle, 2x side, where not threatening to win, put o in corner between the 2 x
+# if 1st o between 2 xs, put o anywhere
+# if 2xs on adjacent sides, put o in either courner, where it touches x
+# if x1 center, x2 corner, put o in corner
+
+
+# move 3
+
